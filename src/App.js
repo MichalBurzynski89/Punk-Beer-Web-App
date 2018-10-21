9bccd1e181edc7e8dacd3aa70afc55fa8349688f
Michał Burzyński <michal.b89@wp.pl>
2018-10-21 16:36:29 +0200

    Installed react-router-dom, added componentDidMount lifecycle method which is fetching data from the Punk API and then pushing every fetched item into array 'items' of the component state, added Navbar.js, added Navbar.css, deleted App.css, logo.svg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends Component {
+  state = {
+    items: [],
+    isLoaded: false,
+    error: null
+  }
+
+  componentDidMount() {
+    let items = [];
+    fetch('https://api.punkapi.com/v2/beers?page=1&per_page=20')
+      .then(res => {
+        if (res.ok) {
+          this.setState({ isLoaded: true });
+          return res.json();
+        } else {
+          throw new Error(`The connection ended with status ${res.status}${res.statusText ? ': ' + res.statusText : ''}`);
+        }
+      })
+      .then(json => {
+        json.forEach(item => {
+          const beer = {
+            id: item.id,
+            name: item.name,
+            tagline: item.tagline,
+            firstBrewed: item.first_brewed,
+            desc: item.description,
+            imageURL: item.image_url
+          };
+          items.push(beer);
+        });
+        this.setState({ items });
+      })
+      .catch(error => this.setState({ isLoaded: true, error }));
+  }
+
   render() {
+    // const { items, isLoaded, error } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          {/* <h1>{error ? `Error: ${error.message}` : (
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                {item.name} {item.tagline}
+              </li>
+            ))}
+          </ul>
+        )}</h1> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <header className="App__header">
+      <h1 className="App__title">Who wants some beer?</h1>
+      <nav className="App__nav">
+        <ul className="menu-list">
+          <li className="menu-list__item">
+            <NavLink className="menu-list__link" to="/">Home</NavLink>
+          </li>
+          <li className="menu-list__item">
+            <NavLink className="menu-list__link" to="/">Favourites</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
