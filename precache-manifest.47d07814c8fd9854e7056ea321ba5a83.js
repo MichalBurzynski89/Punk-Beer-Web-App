80392ae4ede9b1370fc67ad4c19e30c9e0a9b5a8
Michał Burzyński <michal.b89@wp.pl>
2018-10-28 17:33:02 +0100

    Updates

---
+++ precache-manifest.47d07814c8fd9854e7056ea321ba5a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a68c63021dcbdc6299d",
+    "url": "/Punk-Beer-Web-App/static/js/runtime~main.4a68c630.js"
+  },
+  {
+    "revision": "25d06d440509e2f9dd26",
+    "url": "/Punk-Beer-Web-App/static/js/main.25d06d44.chunk.js"
+  },
+  {
+    "revision": "4c0b3fa308eb52eb8fe5",
+    "url": "/Punk-Beer-Web-App/static/js/1.4c0b3fa3.chunk.js"
+  },
+  {
+    "revision": "25d06d440509e2f9dd26",
+    "url": "/Punk-Beer-Web-App/static/css/main.c4441ca0.chunk.css"
+  },
+  {
+    "revision": "4830c4d2c885fa4956f381259b263f1b",
+    "url": "/Punk-Beer-Web-App/index.html"
+  }
+];
